cc87c39831de70b4da48f0367ec932d7ec901c57
Matheus Souza <mvs@mvsouza.com>
2018-12-23 21:40:40 -0200

    DiffControllerTest refactor.

--- test/JsonDiffer.UnitTest/API/DiffControllerTests.cs
+++ test/JsonDiffer.UnitTest/API/DiffControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,24 +14,33 @@ namespace JsonDiffer.UnitTest.API
 {
     public class DiffControllerTests
     {
+        private Mock<IMediator> _mediator;
+        private string _json =  "{\"some\":\"json\"}";
+        private Expression<Func<IMediator, Task>> _mediatorSendMessage = m => m.Send(It.IsAny<IRequest>(), It.IsAny<CancellationToken>());
+        public DiffControllerTests()
+        {
+            _mediator = new Mock<IMediator>();
+        }
         [Fact]
         public async Task Should_send_successfuly_a_command_requestAsync()
         {
-            var mediator = new Mock<IMediator>();
-            var controller = new DiffController(mediator.Object);
-            var result = await controller.PostLeft("teste", "{\"some\":\"json\"}");
-            mediator.Verify(m => m.Send(It.IsAny<IRequest>(),It.IsAny<CancellationToken>()), Times.Once);
+            var result = await PostLeft();
             Assert.True(result is OkResult);
         }
         [Fact]
         public async Task Should_try_to_send_a_command_request_and_handle_a_exceptionAsync()
         {
-            var mediator = new Mock<IMediator>();
-            mediator.Setup(m => m.Send(It.IsAny<IRequest>(), It.IsAny<CancellationToken>())).Throws(new Exception("Left side already filed."));
-            var controller = new DiffController(mediator.Object);
-            var result = await controller.PostLeft("teste", "{\"some\":\"json\"}");
-            mediator.Verify(m => m.Send(It.IsAny<IRequest>(), It.IsAny<CancellationToken>()), Times.Once);
+            _mediator.Setup(_mediatorSendMessage).Throws(new Exception("Left side already filed."));
+            var result = await PostLeft();
             Assert.True(result is BadRequestObjectResult);
+        }
+
+        private async Task<IActionResult> PostLeft()
+        {
+            var controller = new DiffController(_mediator.Object);
+            var result = await controller.PostLeft("teste", _json);
+            _mediator.Verify(_mediatorSendMessage, Times.Once);
+            return result;
         }
     }
 }
