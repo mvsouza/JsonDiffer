168de0b2b1988fe1718e34a483560532aa48ec71
Matheus Souza <mvs@mvsouza.com>
2018-12-24 11:08:15 -0200

    Testing commmand validations.

---
+++ test/JsonDiffer.UnitTest/Application/Validation/PushLeftJsonCommandValidationTests.cs
@@ -0,0 +1,26 @@
+﻿using JsonDiffer.Application.Command;
+using JsonDiffer.Application.Validation;
+using Xunit;
+
+namespace JsonDiffer.UnitTest.Application.Validation
+{
+    public class PushLeftJsonCommandValidationTests
+    {
+        [Fact]
+        public void Should_return_invalid_when_json_is_empty()
+        {
+            var validation = new PushLeftJsonCommandValidation();
+            var validationObj = new PushLeftJsonCommand("ID","");
+            var result = validation.Validate(validationObj);
+            Assert.False(result.IsValid);
+        }
+        [Fact]
+        public void Should_return_invalid_when_id_is_empty()
+        {
+            var validation = new PushLeftJsonCommandValidation();
+            var validationObj = new PushLeftJsonCommand("", "json");
+            var result = validation.Validate(validationObj);
+            Assert.False(result.IsValid);
+        }
+    }
+}
---
+++ test/JsonDiffer.UnitTest/Application/Validation/PushRightJsonCommandValidationTests.cs
@@ -0,0 +1,26 @@
+﻿using JsonDiffer.Application.Command;
+using JsonDiffer.Application.Validation;
+using Xunit;
+
+namespace JsonDiffer.UnitTest.Application.Validation
+{
+    public class PushRightJsonCommandValidationTests
+    {
+        [Fact]
+        public void Should_return_invalid_when_json_is_empty()
+        {
+            var validation = new PushRightJsonCommandValidation();
+            var validationObj = new PushRightJsonCommand("ID", "");
+            var result = validation.Validate(validationObj);
+            Assert.False(result.IsValid);
+        }
+        [Fact]
+        public void Should_return_invalid_when_id_is_empty()
+        {
+            var validation = new PushRightJsonCommandValidation();
+            var validationObj = new PushRightJsonCommand("", "json");
+            var result = validation.Validate(validationObj);
+            Assert.False(result.IsValid);
+        }
+    }
+}
